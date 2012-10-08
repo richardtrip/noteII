1b7cae1ce02cb2eb2a475b83712a3b146aa17db1
Richard Trip <richard.trip@gmail.com>
2012-10-08 19:28:15 +0200

    Initial Samsung Source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Oct 8 19:19:21 CEST 2012"
+#define LINUX_COMPILE_BY "trip"
+#define LINUX_COMPILE_HOST "trip-pc"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
